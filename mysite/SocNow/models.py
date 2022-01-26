f5692e2dd789fab8f9f8d71b7e3cd99715cc5e01
Shahzaib2028 <mohammadshahzaib2028@gmail.com>
2022-01-26 17:52:54 +0500

    dump json

--- mysite/SocNow/models.py
+++ mysite/SocNow/models.py
@@ -1,3 +1,20 @@
+from ssl import Options
+from statistics import mode
 from django.db import models
+from django_mysql.models import ListCharField
 
 # Create your models here.
+class SoC(models.Model):
+    isa = models.IntegerField()
+    extensions = ListCharField(
+                base_field=models.CharField(max_length=50),
+                max_length=100,
+                
+            )
+    devices = ListCharField(
+                base_field=models.CharField(max_length=50),
+                max_length=100,
+                null=True,
+                blank=True
+                            )
+    bus = models.CharField(max_length=50, null=True, blank=True)
