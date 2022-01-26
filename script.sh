94baa67891c9062d909bf911aa9e2c6e454e9b76
Talha Ahmed <talhaahmedashraf@gmail.com>
2022-01-26 18:48:40 +0500

    update sh script

--- script.sh
+++ script.sh
@@ -1,13 +1,13 @@
     #!/bin/bash
     # Ask the user for their name
     echo Which Test you want to run?
-    read test_name
-    echo Running $test_name ...
+    # read test_name
+    echo Running $1 ...
     SocNow_path=$PWD
     export NUCLEUSRV=$SocNow_path/SoCNow/nucleusrv
     cd $SocNow_path/riscv-arch-test
-    make clean TARGETDIR=$NUCLEUSRV/riscv-target RISCV_TARGET=nucleusrv RISCV_DEVICE=rv32i RISCV_ISA=rv32i RISCV_TEST=$test_name TARGET_SIM=$NUCLEUSRV/test_run_dir/Top_Test/VTop
-    make TARGETDIR=$NUCLEUSRV/riscv-target RISCV_TARGET=nucleusrv RISCV_DEVICE=rv32i RISCV_ISA=rv32i RISCV_TEST=$test_name TARGET_SIM=$NUCLEUSRV/test_run_dir/Top_Test/VTop | tee Test_result.txt
+    make clean TARGETDIR=$NUCLEUSRV/riscv-target RISCV_TARGET=nucleusrv RISCV_DEVICE=rv32i RISCV_ISA=rv32i RISCV_TEST=$1 TARGET_SIM=$NUCLEUSRV/test_run_dir/Top_Test/VTop
+    make TARGETDIR=$NUCLEUSRV/riscv-target RISCV_TARGET=nucleusrv RISCV_DEVICE=rv32i RISCV_ISA=rv32i RISCV_TEST=$1 TARGET_SIM=$NUCLEUSRV/test_run_dir/Top_Test/VTop | tee Test_result.txt
     cd ..
     exec bash
 
