d44ebf515730bded7dea28d3b6363e740fc03cd2
shahzaibk23 <shahzaibceo@gmail.com>
2022-02-03 15:59:08 +0500

    admin registered

--- socnow_backend/SocNow/admin.py
+++ socnow_backend/SocNow/admin.py
@@ -1,5 +1,6 @@
 from re import I
 from django.contrib import admin
-from .models import SoC
+from .models import SoC, ComplianceTests
 # Register your models here.
 admin.site.register(SoC)
+admin.site.register(ComplianceTests)
